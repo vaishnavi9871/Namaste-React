e7243e46302eb9267aa4d17fac96e9c126f9ddf3
vaishnavi agrahari <vaishnavi1.agrahari@ril.com>
2026-01-06 12:18:34 +0530

    Episode-01

---
+++ App.js
@@ -0,0 +1,11 @@
+ const heading= React.createElement('h1', {id:"heading",xyz:"abc"} , "Hello World! from React");
+console.log(heading); 
+ const root= ReactDOM.createRoot(document.getElementById('root'));
+    root.render(heading);
+
+const parent= React.createElement('div', {id:"parent"},
+React.createElement('div', {id:"child"}, 
+[React.createElement('h1', {}, "I am h1 tag from child 1"),
+   React.createElement('h2', {}, "I am h2 tag from child 1")
+],
+));
